60358e03c4069234e7cac54880d0562358f7419e
Batuhan <43238479+batuhanoezcan@users.noreply.github.com>
2020-03-09 15:38:59 +0100

    Update main.java

--- DecoratorPattern/src/main.java
+++ DecoratorPattern/src/main.java
@@ -3,11 +3,8 @@ public class main {
 
 	public static void main(String[] args) {
 
-		Schnitzel k = new KetchupDecorator(new Wienerschnitzel());
-		k.draw();
-		System.out.println(k.givePrice() + " Euro");
-		
-		Schnitzel a = new SenfDecorator(new Rinderschnitzel());
+		Schnitzel k = new KetchupDecorator(new Wienerschnitzel());		
+		Schnitzel a = new SenfDecorator(k);
 		a.draw();
 		System.out.println(a.givePrice() + " Euro");
 	}
